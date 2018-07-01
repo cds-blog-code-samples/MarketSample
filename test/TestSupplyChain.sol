35c77b56aa3700e3ccec048efddcf0b62fa399e0
Amal Sudama <cds.sudama@gmail.com>
2018-07-01 06:55:51 -0400

    first attempt

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -1,13 +1,40 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.21;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
+
 contract TestSupplyChain {
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
+
+    function testItemCanBePutOnSale() public {
+        // SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
+        SupplyChain supplyChain = new SupplyChain();
+        supplyChain.addItem("Gem", 108);
+
+        uint forSale = 0;
+
+        (
+        string memory name, uint sku, uint price,
+        uint state, address seller, address buyer
+        )  = supplyChain.fetchItem(0);
+
+        Assert.equal(state, forSale, "Initial item is for sale");
+    }
+
+    function testThatUserNeedsEnoughFunds() public {
+        SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
+        supplyChain.addItem("Gem", 108);
+
+        uint sku = 0;
+        // bool result = supplyChain.call.value(200).gas(80000 wei)(bytes4(keccak256("buyItem(uint)")), sku);
+        bool result = address(supplyChain).call.value(2000)(abi.encodeWithSignature("buyItem(uint)", sku));
+
+        Assert.isFalse(result, "should not be able to purchase item with less funds than required");
+    }
 
     // buyItem
 
@@ -25,7 +52,7 @@ contract TestSupplyChain {
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
 
-     
+
 
 
 }
