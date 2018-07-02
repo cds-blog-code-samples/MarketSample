f650d1b73b74273705e4a425669cc2b5bb752714
Amal Sudama <cds.sudama@gmail.com>
2018-07-02 10:02:57 -0400

    wip

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,96 +5,61 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 
-contract ThrowProxy {
-    address public target;
-    bytes data;
-    uint value;
-
-    constructor(address _target, uint _value) public {
-        target = _target;
-        value = _value;
-    }
-
-    // prime the data using the fallback function.
-    function() public {
-        data = msg.data;
-    }
-
-    function execute() public payable returns (bool) {
-        return target.call.value(value)(data);
-    }
-}
-
-
 contract TestSupplyChain {
-    function initializeContract()
-    public
-    returns (SupplyChain)
+    // uint public initialBalance = 100;
+    uint public initialBalance;
+    SupplyChain public chain;
+
+    string itemName = "Gem";
+    uint   itemPrice = 3;
+
+    function beforeEach() public
     {
-        return new SupplyChain();
+        chain = new SupplyChain();
+        initialBalance = 100;
     }
 
-    function putItemForSale()
-    public
-    returns (SupplyChain)
+    function putItemForSale() public
     {
-        SupplyChain chain = initializeContract();
-        chain.addItem("Gem", 108);
-        return chain;
+        chain.addItem(itemName, itemPrice);
     }
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
     function testItemCanBePutOnSale() public {
-        SupplyChain chain = putItemForSale();
+        putItemForSale();
 
         uint expectedState = 0; // 0: Sale
-        uint expectedPrice = 108;
         uint expectedSku = 0;
-        string memory expectedName = "Gem";
         address expectedBuyer = address(0);
-        address expectedSeller = address(msg.sender);
+        address expectedSeller = this;
 
         (
         string memory name, uint sku, uint price, uint state, address seller, address buyer
-        )  = chain.fetchItem(0);
+        )  = chain.fetchItem(expectedSku); // the first item
 
+        Assert.equal(sku, expectedSku, "Item sku is correct");
+        Assert.equal(name, itemName, "Item is named correctly");
+        Assert.equal(price, itemPrice, "Item price correctly");
         Assert.equal(state, expectedState, "Item State is `For sale`");
-        Assert.equal(name, expectedName, "Item is named correctly");
-        Assert.equal(sku, expectedSku, "Item sku is 0");
-        Assert.equal(price, expectedPrice, "Item sku is 0");
         Assert.equal(buyer, expectedBuyer, "Item buyer is 0x0");
-
-        // Todo: how to test for expected seller?
-        // Which account is used by the test runner?
-        // Assert.equal(seller, expectedSeller, "Item seller is me");
+        Assert.equal(seller, expectedSeller, "Item seller is me");
     }
+
+    function testUserDoesNotPaysTheRightPrice() public payable {
+        uint sku = 0;
+        bool result = address(chain).call.value(0)(abi.encodeWithSignature("buyItem(uint)", sku));
+        Assert.isFalse(result, "under Paid for item");
+    }
+
 
     function testUserPaysTheRightPrice() public {
-        SupplyChain chain = putItemForSale();
-        ThrowProxy proxy = new ThrowProxy(address(chain), 2009);
+        SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
+        putItemForSale();
         uint sku = 0;
-        SupplyChain(address(proxy)).buyItem(sku);
-        bool result = proxy.execute.gas(5100000 wei)();
-        // bool result = address(chain).call.value(2000)(abi.encodeWithSignature("buyItem(uint)", sku));
-        Assert.isFalse(result, "should not be able to purchase item with less funds than required");
+        bool result = address(supplyChain).call.value(itemPrice)(abi.encodeWithSignature("buyItem(uint)", sku));
+        Assert.isTrue(result, "Paid the correct price...");
     }
-
-// function testUserPaysTheRightPrice() public {
-        // SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
-        // supplyChain.addItem("Gem", 108);
-        // uint sku = 0;
-        // bool result = address(supplyChain).call.value(2000)(abi.encodeWithSignature("buyItem(uint)", sku));
-        // Assert.isFalse(result, "should not be able to purchase item with less funds than required");
-    // }
-
-    // function testUserPaysTheRightPrice() public {
-        // SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
-        // supplyChain.addItem("Gem", 108);
-        // uint sku = 0;
-        // bool result = address(supplyChain).call.value(2000)(abi.encodeWithSignature("buyItem(uint)", sku));
-        // Assert.isFalse(result, "should not be able to purchase item with less funds than required");
-    // }
 
     // buyItem
 
