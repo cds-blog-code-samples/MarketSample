591998cca20f3048ebfbaefb8a2e8ba1401e2489
Amal Sudama <cds.sudama@gmail.com>
2018-07-03 07:33:04 -0400

    refactor state validation

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -46,6 +46,22 @@ contract TestSupplyChain {
         Assert.equal(seller, expectedSeller, "Item seller is me");
     }
 
+
+    function validateCurrentSkuState(uint _expectedSku, uint _expectedState)
+        public
+        returns (bool)
+    {
+        string memory name;
+        uint sku;
+        uint price;
+        uint state;
+        address seller;
+        address buyer;
+
+        ( name, sku, price, state, seller, buyer) = chain.fetchItem(_expectedSku);
+        return state == _expectedState;
+    }
+
     // buyItem
     // test for failure if user does not send enough funds
     function testUserDoesNotPaysTheRightPrice() public {
@@ -55,6 +71,10 @@ contract TestSupplyChain {
         // solhint-disable-next-line
         bool result = address(chain).call.value(offer)(abi.encodeWithSignature("buyItem(uint256)", sku));
         Assert.isFalse(result, "under Paid for item");
+
+        // Verify state is For Sale
+        uint expectedState = 0; // For Sale
+        Assert.isTrue(validateCurrentSkuState(sku, expectedState), "Item should be `for sale`");
     }
 
     // buyItem
@@ -66,6 +86,10 @@ contract TestSupplyChain {
         // solhint-disable-next-line
         bool result = address(chain).call.value(offer)(abi.encodeWithSignature("buyItem(uint256)", sku));
         Assert.isTrue(result, "Paid the correct price...");
+
+        // Verify state is Sold
+        uint expectedState = 1; // Sold
+        Assert.isTrue(validateCurrentSkuState(sku, expectedState), "Item should be `Sold`");
     }
 
     // shipItem
@@ -85,12 +109,16 @@ contract TestSupplyChain {
         // todo: what's the difference in using callcode?
         bool result = address(proxy).delegatecall(abi.encodeWithSignature("ship(uint256)", sku));
         Assert.isFalse(result, "Non seller cannot ship");
+
+        // Verify state remains Sold
+        uint expectedState = 1; // Sold
+        Assert.isTrue(validateCurrentSkuState(sku, expectedState), "Item should be `Sold`");
     }
 
     // shipItem
     // only seller can ship
     function testShipWhenCalledBySeller() public {
-        uint expectedSku = 0;
+        uint sku = 0;
 
         // Purchase item
         uint offer = itemPrice + 1; // exceed price
@@ -98,52 +126,36 @@ contract TestSupplyChain {
         // solhint-disable-next-line
         address(chain).call.value(offer)(abi.encodeWithSignature("buyItem(uint256)", sku));
 
-        // try to ship item
-        bool result = address(chain).call(abi.encodeWithSignature("shipItem(uint256)", expectedSku));
+        // Try to ship item
+        bool result = address(chain).call(abi.encodeWithSignature("shipItem(uint256)", sku));
         Assert.isTrue(result, "Only seller can ship");
 
-        // verify state is Shipped
-        uint expectedState = 2; // 2: Shipped
-
-        string memory name;
-        uint sku;
-        uint price;
-        uint state;
-        address seller;
-        address buyer;
-
-        ( name, sku, price, state, seller, buyer) = chain.fetchItem(expectedSku);
-        Assert.equal(state, expectedState, "Item State is `Shipped`");
+        // Verify state is Shipped
+        uint expectedState = 2; // Shipped
+        Assert.isTrue(validateCurrentSkuState(sku, expectedState), "Item is `Shipped`");
     }
 
     // shipItem
     // test for trying to ship an item that is not marked Sold
     function testCannotShipAnItemThatIsNotSold() public {
-        uint expectedSku = 0;
+        uint sku = 0;
 
         // item is in forSale state
 
         // try to ship item
-        bool result = address(chain).call(abi.encodeWithSignature("shipItem(uint256)", expectedSku));
+        bool result = address(chain).call(abi.encodeWithSignature("shipItem(uint256)", sku));
         Assert.isFalse(result, "Cannot ship item not sold.");
 
-        // verify state is Shipped
-        uint expectedState = 0; // 2: ForSale
-
-        string memory name;
-        uint sku;
-        uint price;
-        uint state;
-        address seller;
-        address buyer;
-
-        ( name, sku, price, state, seller, buyer) = chain.fetchItem(expectedSku);
-        Assert.equal(state, expectedState, "Item State is `For sale`");
+        // Verify state is For Sale
+        uint expectedState = 0; // For Sale
+        Assert.isTrue(validateCurrentSkuState(sku, expectedState), "Item should be `for sale`");
     }
 
     // receiveItem
-
     // test calling the function from an address that is not the buyer
+
+
+    // receiveItem
     // test calling the function on an item not marked Shipped
 
 }
