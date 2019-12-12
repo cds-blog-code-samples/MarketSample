fbb2e3f0061943debad25185f3d3240c498a4b90
Amal Sudama <cds.sudama@gmail.com>
2019-12-12 14:44:30 -0500

    not really relevant commit, to force a teams update

--- contracts/SupplyChainState.sol
+++ contracts/SupplyChainState.sol
@@ -1,6 +1,10 @@
 pragma solidity >= 0.5.0 < 0.6.0;
 
 
+// Comments don't get more random than this, huh?
+
+
+/// The different states of our supply chain
 contract SupplyChainState {
     enum State { ForSale, Sold, Shipped, Received }
 }
