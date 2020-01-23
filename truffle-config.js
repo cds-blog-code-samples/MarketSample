5edbdc8d51ce289bcef52deb95950a71c8678de3
Amal Sudama <cds.sudama@gmail.com>
2020-01-23 15:03:28 -0500

    configure for ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC
+const ropsten_endpoint = process.env.ROPSTEN_ENDPOINT
+
 module.exports = {
 
   compilers: {
@@ -11,6 +15,15 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic,ropsten_endpoint),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
   }
 };
